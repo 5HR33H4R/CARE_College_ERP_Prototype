a1be74b5a03c2a0cc8afd0f3493ac722288cf425
5HR33H4R <shreehar3400k.e@gmail.com>
2023-03-10 08:42:19 +0530

    converted the code into flask app

--- app.py
+++ app.py
@@ -2,9 +2,21 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-	return render_template("index.html")
+    return render_template("index.html")
+
+
+@app.route("/add_student")
+def add_student():
+    return render_template("add_student.html")
+
+
+@app.route("/edit")
+def edit():
+    return render_template("edit.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
